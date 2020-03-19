855258a8d94d04e0cbbdfe608e9e61f973a658fa
Tahasiin <tahasinislam39@gmail.com>
2020-03-20 01:13:22 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+
+if (isset($_POST['submit'])) {
+    $_SESSION['code'] = $_POST['code'];
+}
+if (isset($_SESSION['code'])) {
+    $code = $_SESSION['code'];
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="" href="https://img.icons8.com/cotton/2x/source-code--v2.png" />
+        <title>Simple Snippet</title>
+        <link href="https://fonts.googleapis.com/css?family=Oxanium&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+            textarea{
+                font-family: 'Oxanium', cursive;
+
+            }
+            button{
+                color:blueviolet;
+                cursor: pointer
+            }
+        </style>
+    </head>
+    <body>
+
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <button type="submit" name="submit">RUN <i class="fa fa-play" aria-hidden="true"></i></button>
+            <a href="result.php?fullscreen=1" title="Full Screen"><i class="fa fa-window-maximize" aria-hidden="true"></i></a>
+            <textarea name="code" spellcheck="false" style="width: 100%" rows="18" placeholder="Type or paste your html,css,javascript code here....."><?php
+                if (!isset($_SESSION['code'])) {
+                    
+                } else {
+                    echo $code;
+                }
+                ?></textarea>
+        </form>
+
+        <iframe src="result.php" style="width: 100%;height: 300px"></iframe>
+        
+        <script>
+            $(document).ready(function () {
+                $("#button").click(function () {
+                    var message_to = $("#message_to").val();
+                    var message = $("#message").val();
+                    $.ajax({
+                        url: 'post.php',
+                        method: 'POST',
+                        data: {
+                            message_to: message_to,
+                            message: message
+                        },
+                        success: function (data) {
+                            $("#message").val('');
+                        }
+                    });
+                });
+            });
+        </script>
+        <script>
+            if (window.history.replaceState) {
+                window.history.replaceState(null, null, window.location.href);
+            }
+        </script>
+    </body>
+</html>
+
